a7de4808968475315994ffe2a3e2469bb71ee003
Riyad Shauk <riyad.shauk@gmail.com>
2019-01-15 23:42:13 -0800

    fix bug: make non-blocking

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,8 +1,8 @@
 const constants = {};
-constants.port = process.env.PORT || process.env.NODE_ENV === 'production' ? 80 : 8080;
+constants.port = process.env.PORT ? process.env.PORT : 8080;
 constants.pathPrefix = ''; // in case we want to treat a specific path as the root (ie: riyadshauk.com/photos -> pathPrefix = '/photos')
 constants.test = { // to the world: this is just for testing : P
   user: 'test',
   passwordHash: '78bbf9c8a9f5d4ae32365449d21c3830220a776f583fd923c1187e4bfcfe5d4c'
-}
+};
 module.exports = constants;
